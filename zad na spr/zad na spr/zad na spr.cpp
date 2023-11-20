71faa134e18d0a44f98e1210b26df97161ed51d6
unknown <konrad.bienkowski1@o2.pl>
2023-11-20 18:23:05 +0100

    nasprsigma

---
+++ zad na spr/zad na spr/zad na spr.cpp
@@ -0,0 +1,38 @@
+﻿#include <fstream>
+#include <string>
+#include <iostream>
+using namespace std;
+int main()
+{
+    fstream plik;
+    string linia;
+    int Sumal = 0;
+    int sumalp = 0;
+    plik.open("dane.txt", ios::in);
+
+    if (plik.good() == true) { //suma wszytskich liczb (podzielnych przez 1 (%) - dzielenie
+        cout << "W danej lokalizacji znajduje się plik tekstowy 'notka.txt'" << endl;
+        while (getline(plik, linia)) {
+            if (stoi(linia) % 1 == 0) {
+                Sumal = Sumal + stoi(linia);
+            }
+        }
+        cout << "suma liczb = " << Sumal << endl;
+            }/* //suma liczb parzystych i innych
+            if (stoi(linia) % 2 == 0) {
+                sumalp = sumalp + stoi(linia);
+            }
+            else {
+                Sumal = Sumal + stoi(linia);
+            }
+        }
+        cout << "suma liczb parzystych = " << sumalp << endl;
+        cout << "suma liczb innych = " << Sumal << endl;
+    }*/
+    else {
+        cout << "nie ma file" << endl;
+    }
+    plik.close();
+    return 0;
+
+}
